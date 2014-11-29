4dbd0ad74ace2cb07a59fb8a9e6ad0f16fc0a7b3
HalfLike <luqingcuihust@gmail.com>
2014-11-29 13:42:05 +0800

    modify

--- app/views.py
+++ app/views.py
@@ -1,6 +1,28 @@
-from flask import Flask, jsonify
 from app import app
-
+from flask import Flask, jsonify, abort, request, make_response, url_for
+from flask.ext.httpauth import HTTPBasicAuth
+ 
+auth = HTTPBasicAuth()
+ 
+@auth.get_password
+def get_password(username):
+    if username == 'lu':
+        return 'python'
+    return None
+ 
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify( { 'error': 'Unauthorized access' } ), 401)
+    # return 403 instead of 401 to prevent browsers from displaying the default auth dialog
+    
+@app.errorhandler(400)
+def not_found(error):
+    return make_response(jsonify( { 'error': 'Bad request' } ), 400)
+ 
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify( { 'error': 'Not found' } ), 404)
+ 
 tasks = [
     {
         'id': 1,
@@ -14,8 +36,69 @@ tasks = [
         'description': u'Need to find a good Python tutorial on the web', 
         'done': False
     }
-]   
-
-@app.route('/api/tasks', methods=['GET'])
+]
+ 
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id = task['id'], _external = True)
+        else:
+            new_task[field] = task[field]
+    return new_task
+    
+@app.route('/api/tasks', methods = ['GET'])
+@auth.login_required
 def get_tasks():
-    return jsonify({'tasks': tasks})
+    return jsonify( { 'tasks': map(make_public_task, tasks) } )
+ 
+@app.route('/api/tasks/<int:task_id>', methods = ['GET'])
+@auth.login_required
+def get_task(task_id):
+    task = filter(lambda t: t['id'] == task_id, tasks)
+    if len(task) == 0:
+        abort(404)
+    return jsonify( { 'task': make_public_task(task[0]) } )
+ 
+@app.route('/api/tasks', methods = ['POST'])
+@auth.login_required
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify( { 'task': make_public_task(task) } ), 201
+ 
+@app.route('/api/tasks/<int:task_id>', methods = ['PUT'])
+@auth.login_required
+def update_task(task_id):
+    task = filter(lambda t: t['id'] == task_id, tasks)
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify( { 'task': make_public_task(task[0]) } )
+    
+@app.route('/api/tasks/<int:task_id>', methods = ['DELETE'])
+@auth.login_required
+def delete_task(task_id):
+    task = filter(lambda t: t['id'] == task_id, tasks)
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify( { 'result': True } )
+    
--- run.py
+++ run.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
 #! -*- coding:utf-8 -*-
-from app import app
+from app import app, url_for
 
 @app.route('/')
 def index():
     return "<span style='color:red'>Welcom to HalfLike's world!</span>"
+
+@app.route('/pr')
+def pr():
+    return url_for('pr', aa=1, _external=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
